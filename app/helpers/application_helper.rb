8c9a7f5058cf7e338c733a2b75fb61f4a6b576ea
kip <cyruskiprop254@gmail.com>
2020-05-07 16:24:57 +0300

    add vote associations and vote/disvote fuctionality

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+   def upvote_or_downvote_btn(article)
+    vote = Vote.find_by(article: article, user: current_user)
+    if vote
+      link_to('Disvote!', article_vote_destroy_path({id: vote.id}), method: :delete)
+    else
+      link_to('vote!', article_vote_create_path({id: article.id}), method: :post)
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :articles
+  resources :votes
+  post 'articles/vote/id', to: 'votes#create', as: 'article_vote_create'
+  delete 'articles/votes/:id', to: 'votes#destroy', as: 'article_vote_destroy'
   devise_for :users, controllers:  { registrations: 'registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'articles#index'
