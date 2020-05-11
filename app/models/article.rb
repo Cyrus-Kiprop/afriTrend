f35ea5250922c2a9269987ab3a54a37fb0b5f233
kip <cyruskiprop254@gmail.com>
2020-05-12 00:56:30 +0300

    validate User, Vote, Article and Category models

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,15 +4,19 @@ class Article < ApplicationRecord
   has_many :votes, dependent: :destroy
   has_and_belongs_to_many :categories
 
+  validates :user_id, presence: true, numericality: { only_integer: true }
+  validates :title, presence: true, length: { maximum: 140 }
+  validates :content, presence: true, length: { minimum: 25 }
+
   def self.top_voted_article(num = 1)
-    select("articles.*, count(votes.id) AS votes_count")
+    select('articles.*, count(votes.id) AS votes_count')
       .joins(:votes)
-      .group("articles.id")
-      .order("votes_count DESC")
+      .group('articles.id')
+      .order('votes_count DESC')
       .limit(num)
   end
 
   def self.get_category_based_articles(category)
-    joins(:categories).where("categories.name = ?", category)
+    joins(:categories).where('categories.name = ?', category)
   end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ApplicationRecord
+  validates :name, presence: true
+  validates :priority, presence: true, numericality: { only_integer: true }
   has_and_belongs_to_many :articles
 end
