986ea2b59241bdfe289d67558dff58b2946c4d97
kip <cyruskiprop254@gmail.com>
2020-05-13 01:05:11 +0300

    finish on databases

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,34 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+
   def show
     @user = User.find(params[:id])
+  end
+
+  def create
+    vote = current_user.votes.build(article_id: vote_params)
+
+    if vote.save
+      flash[:success] = 'Successfully voted'
+      redirect_back fallback_location: root_path
+    else
+      flash[:error] = 'Internal server error'
+    end
+  end
+
+  def destroy
+    vote = Vote.find_by_id(vote_params)
+    if vote.destroy
+      flash[:success] = 'Successfully unvoted'
+      redirect_back fallback_location: root_path
+    else
+      flash[:error] = 'Internal server error'
+    end
+  end
+
+  private
+
+  def vote_params
+    params.require(:id)
   end
 end
